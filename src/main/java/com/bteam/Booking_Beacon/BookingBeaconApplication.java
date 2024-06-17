0240a4fad923787e983d1193d502f11df4480fbd
Olllieee <roditee@naver.com>
2024-06-17 10:51:28 +0900

    init

---
+++ src/main/java/com/bteam/Booking_Beacon/BookingBeaconApplication.java
@@ -0,0 +1,13 @@
+package com.bteam.Booking_Beacon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingBeaconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingBeaconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bteam/Booking_Beacon/BookingBeaconApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bteam.Booking_Beacon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingBeaconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
