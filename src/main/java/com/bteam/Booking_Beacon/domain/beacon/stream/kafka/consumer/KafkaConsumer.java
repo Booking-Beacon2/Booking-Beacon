e6378fdd8e761aa6036ee3a0a8e58bf10573ecbc
jjmmll0727 <jjmmll0727@gmail.com>
2024-06-20 14:16:02 +0900

    fsf

--- src/main/java/com/bteam/Booking_Beacon/domain/beacon/stream/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/bteam/Booking_Beacon/domain/beacon/stream/kafka/consumer/KafkaConsumer.java
@@ -1,35 +1,70 @@
-package com.bteam.Booking_Beacon.domain.beacon.stream.kafka.consumer;
-
-import com.bteam.Booking_Beacon.domain.beacon.dto.PickReq;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-@RequiredArgsConstructor
-public class KafkaConsumer {
-
-    private final ObjectMapper objectMapper;
-
-    @KafkaListener(topics = "topic1", groupId = "group1")
-    public void listen1(String message) throws JsonProcessingException {
-
-        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
-        log.info("1111>>>>>>>>>");
-        log.info(String.valueOf(pickReq.getBeaconId()));
-    }
-
-    @KafkaListener(topics = "topic2", groupId = "group1")
-    public void listen2(String message) throws JsonProcessingException {
-
-        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
-        log.info("2222>>>>>>>>>");
-        log.info(String.valueOf(pickReq.getBeaconId()));
-    }
-}
-
-
+//package com.bteam.Booking_Beacon.domain.beacon.stream.kafka.consumer;
+//
+//import com.bteam.Booking_Beacon.domain.beacon.dto.PickReq;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//@RequiredArgsConstructor
+//public class KafkaConsumer {
+//
+//    private final ObjectMapper objectMapper;
+//
+//    @KafkaListener(topics = "topic1", groupId = "group1")
+//    public void listen1(String message) throws JsonProcessingException {
+//
+//        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
+//        log.info("1111>>>>>>>>>");
+//        log.info(String.valueOf(pickReq.getBeaconId()));
+//    }
+//
+//    @KafkaListener(topics = "topic2", groupId = "group1")
+//    public void listen2(String message) throws JsonProcessingException {
+//
+//        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
+//        log.info("2222>>>>>>>>>");
+//        log.info(String.valueOf(pickReq.getBeaconId()));
+//    }
+//}
+//
+//
+//package com.bteam.Booking_Beacon.domain.beacon.stream.kafka.consumer;
+//
+//import com.bteam.Booking_Beacon.domain.beacon.dto.PickReq;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//@RequiredArgsConstructor
+//public class KafkaConsumer {
+//
+//    private final ObjectMapper objectMapper;
+//
+//    @KafkaListener(topics = "topic1", groupId = "group1")
+//    public void listen1(String message) throws JsonProcessingException {
+//
+//        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
+//        log.info("1111>>>>>>>>>");
+//        log.info(String.valueOf(pickReq.getBeaconId()));
+//    }
+//
+//    @KafkaListener(topics = "topic2", groupId = "group1")
+//    public void listen2(String message) throws JsonProcessingException {
+//
+//        PickReq pickReq = objectMapper.readValue(message, PickReq.class);
+//        log.info("2222>>>>>>>>>");
+//        log.info(String.valueOf(pickReq.getBeaconId()));
+//    }
+//}
+//
+//
--- src/main/java/com/bteam/Booking_Beacon/domain/beacon/stream/kafka/producer/KafkaProducer.java
+++ src/main/java/com/bteam/Booking_Beacon/domain/beacon/stream/kafka/producer/KafkaProducer.java
@@ -1,30 +1,30 @@
-package com.bteam.Booking_Beacon.domain.beacon.stream.kafka.producer;
-
-import com.bteam.Booking_Beacon.domain.beacon.dto.PickReq;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-import java.util.Random;
-
-@Component
-@Slf4j
-@RequiredArgsConstructor
-public class KafkaProducer {
-
-    private final static List<String> topicList = List.of("topic1", "topic2");
-    private final KafkaTemplate<String, String> kafkaProducerTemplate;
-    private final ObjectMapper objectMapper;
-
-    public void sendMessage(PickReq pickReq, Long userId) throws JsonProcessingException {
-        log.info("Sending pick request to topic: {} and userId: {}", pickReq, userId);
-        String randomTopic = topicList.get(new Random().nextInt(topicList.size()));
-        String message = objectMapper.writeValueAsString(pickReq);
-
-        this.kafkaProducerTemplate.send(randomTopic, message);
-    }
-}
+//package com.bteam.Booking_Beacon.domain.beacon.stream.kafka.producer;
+//
+//import com.bteam.Booking_Beacon.domain.beacon.dto.PickReq;
+//import com.fasterxml.jackson.core.JsonProcessingException;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//import java.util.Random;
+//
+//@Component
+//@Slf4j
+//@RequiredArgsConstructor
+//public class KafkaProducer {
+//
+//    private final static List<String> topicList = List.of("topic1", "topic2");
+//    private final KafkaTemplate<String, String> kafkaProducerTemplate;
+//    private final ObjectMapper objectMapper;
+//
+//    public void sendMessage(PickReq pickReq, Long userId) throws JsonProcessingException {
+//        log.info("Sending pick request to topic: {} and userId: {}", pickReq, userId);
+//        String randomTopic = topicList.get(new Random().nextInt(topicList.size()));
+//        String message = objectMapper.writeValueAsString(pickReq);
+//
+//        this.kafkaProducerTemplate.send(randomTopic, message);
+//    }
+//}
